c1772e0b3f750eff6e1a4ba08ab010bcfaab7d08
Kamil Podsiadlo <kpodsiadlo@virtuslab.com>
2022-01-06 11:40:37 +0100

    working example

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
-addSbtPlugin("org.scala-native" % "sbt-scala-native" % "0.4.3-RC1")
+// addSbtPlugin("org.scala-native" % "sbt-scala-native" % "0.4.3-RC1")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.8.0")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,19 @@
 import org.virtuslab.yaml.*
 
 object Main extends App {
-  case class Language(kind: String, platform: String) derives YamlCodec
-  val yaml = """|kind: Scala
-                |platform: Native
-                |""".stripMargin
 
-  pprint.pprintln(yaml.as[Language])
+  case class Library(
+    library: String,
+    language: String,
+    version: String,
+    platforms: List[String],
+  ) derives YamlCodec
+
+  val yaml: String = """|library: scala-yaml
+                        |language: Scala
+                        |version: 3.1.0
+                        |platforms: [JVM, JS, Native]
+                        |""".stripMargin
+
+  pprint.pprintln(yaml.as[Library], width = 30)
 }
