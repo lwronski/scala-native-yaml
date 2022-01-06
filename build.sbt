7e879d1796f631d4816c87cf03f0a70968c2cfe2
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-01-06 17:10:45 +0100

    Native benchmark

--- build.sbt
+++ build.sbt
@@ -2,13 +2,13 @@ scalaVersion := "3.1.0"
 
 resolvers += Resolver.sonatypeRepo("snapshots")
 
-// Set to false or remove if you want to show stubs as linking errors
-nativeLinkStubs := true
 
 enablePlugins(ScalaNativePlugin)
 
 libraryDependencies ++= List(
-  "org.virtuslab" %%% "scala-yaml" % "0.0.4+22-d6ccdc59-SNAPSHOT",
+  "org.virtuslab" %%% "scala-yaml" % "0.0.4+23-cb6aab7b-SNAPSHOT",
   "com.lihaoyi" % "pprint_native0.4_2.13" % "0.7.1"
-
 )
+
+
+nativeConfig ~= {_.withMode(scalanative.build.Mode.releaseFull).withLTO(scalanative.build.LTO.thin)}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,18 +3,36 @@ import org.virtuslab.yaml.*
 
 object Main extends App {
 
-  case class Library(
-    library: String,
-    language: String,
-    version: String,
-    platforms: List[String],
-  ) derives YamlCodec
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+}
 
-  val yaml: String = """|library: scala-yaml
-                        |language: Scala
-                        |version: 3.1.0
-                        |platforms: [JVM, JS, Native]
-                        |""".stripMargin
+  case class Web(build: String, ports: List[String], volumes: List[String]) derives YamlCodec
+  case class Redis(image: String) derives YamlCodec
+  case class Services(web: Web, redis: Redis) derives YamlCodec
+  case class Compose(version: String, services: Services) derives YamlCodec
 
-  pprint.pprintln(yaml.as[Library], width = 30)
+  val yamlString =
+    s"""version: "3.9"
+       |services:
+       |  web:
+       |    build: .
+       |    volumes:
+       |      - .:/code
+       |      - logvolume01:/var/log
+       |    ports:
+       |      - "5000:5000"
+       |  redis:
+       |    image: "redis:alpine"
+       |""".stripMargin
+
+  time {
+    for(i <- 1 to 100000) {
+      yamlString.as[Compose].getOrElse(???)
+    }
+  }
 }
